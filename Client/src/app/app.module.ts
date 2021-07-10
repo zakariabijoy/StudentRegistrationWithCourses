4243e802e474c39491a8ece8459cfb40b62a01f8
zakariabijoy <zakaria.bijoy@live.com>
2021-07-10 15:06:25 +0600

    students and student component and student service is added

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentComponent } from './Students/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
